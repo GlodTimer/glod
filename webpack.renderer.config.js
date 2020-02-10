61d19b57c40c77f89537d353735885e50d20a515
Thomas <trasmussen@redventures.com>
2020-02-10 18:20:24 -0500

    adding css modules

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -3,7 +3,16 @@ const plugins = require("./webpack.plugins");
 
 rules.push({
   test: /\.css$/,
-  use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+  use: [
+    "style-loader",
+    "@teamsupercell/typings-for-css-modules-loader",
+    {
+      loader: "css-loader",
+      options: {
+        modules: true
+      }
+    }
+  ]
 });
 
 module.exports = {
